25e7a5368f9681c1a3a660072a39fbd14c9252c3
Timur Karpenok <nbvehbq@gmail.com>
2017-09-12 23:27:58 +0300

    add ChatList class

--- app/scripts/src/dom.js
+++ app/scripts/src/dom.js
@@ -1,4 +1,16 @@
 import $ from 'jquery';
+import md5 from 'crypto-js/md5';
+import moment from 'moment';
+
+function createGravatarUrl(username) {
+  let userhash = md5(username);
+  return `http://www.gravatar.com/avatar/${userhash.toString()}`;
+}
+
+export function promptForUsername() {
+  let username = prompt('Enter a username');
+  return username.toLowerCase();
+}
 
 export class ChatForm {
   constructor(formSel, inputSel) {
@@ -16,5 +28,60 @@ export class ChatForm {
     });
 
     this.$form.find('button').on('click', () => this.$form.submit());
+  }
+}
+
+export class ChatList {
+  constructor(listSel, username) {
+    this.$list = $(listSel);
+    this.username = username;
+  }
+
+  drawMessage({ user: u, timestamp: t, message: m }) {
+    let $messageRow = $('<li>', {
+      'class': 'message-row'
+    });
+
+    if (this.username === u) {
+      $messageRow.addClass('me');
+    }
+
+    let $message = $('<p>');
+
+    $message.append($('<span>', {
+      'class': 'message-username',
+      text: u
+    }));
+
+    $message.append($('<span>', {
+      'class': 'timestamp',
+      'data-time': t,
+      text: moment(t).fromNow()
+    }));
+
+    $message.append($('<span>', {
+      'class': 'message-message',
+      text: m
+    }));
+
+    let $img = $('<img>', {
+      src: createGravatarUrl(u),
+      title: u });
+
+    $messageRow.append($img);
+    $messageRow.append($message);
+    this.$list.append($messageRow);
+    $messageRow.get(0).scrollIntoView();
+  }
+
+  init() {
+    this.timer = setInterval(() => {
+      $('[data-time]').each((idx, element) => {
+        let $element = $(element);
+        let timestamp = new Date().setTime($element.attr('data-time'));
+        let ago = moment(timestamp).fromNow();
+        $element.html(ago);
+      });
+    }, 1000);
   }
 }
